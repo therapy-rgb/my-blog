9b309fee6adbdee1338d36c3c360464cb3067390
therapy-rgb <therapy@marcusberley.com>
2025-09-18 13:51:18 -0400

    Add golf club favicon to Suburban Dad Mode - complete favicon implementation for desktop and mobile

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'my-blog',
+  title: 'Suburban Dad Mode',
 
   projectId: '4qp7h589',
   dataset: 'production',
@@ -15,4 +15,90 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+
+  studio: {
+    components: {
+      navbar: undefined, // Use default navbar
+      layout: undefined, // Use default layout
+    },
+  },
+
+  document: {
+    newDocumentOptions: (prev, {creationContext}) => {
+      if (creationContext.type === 'global') {
+        return prev.filter((template) => template.templateId !== 'settings')
+      }
+      return prev
+    },
+  },
+
+  // Custom favicon and meta tags
+  head: [
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/static/favicon.ico'
+      }
+    },
+    {
+      tag: 'link', 
+      attributes: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/static/favicon-32x32.png'
+      }
+    },
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'icon', 
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/static/favicon-16x16.png'
+      }
+    },
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'apple-touch-icon',
+        sizes: '180x180', 
+        href: '/static/apple-touch-icon.png'
+      }
+    },
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/static/android-chrome-192x192.png'
+      }
+    },
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'icon', 
+        type: 'image/png',
+        sizes: '512x512',
+        href: '/static/android-chrome-512x512.png'
+      }
+    },
+    {
+      tag: 'link',
+      attributes: {
+        rel: 'manifest',
+        href: '/static/site.webmanifest'
+      }
+    },
+    {
+      tag: 'meta',
+      attributes: {
+        name: 'theme-color',
+        content: '#ffffff'
+      }
+    }
+  ],
 })
